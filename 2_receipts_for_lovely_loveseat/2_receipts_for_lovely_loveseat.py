d511a6758d0cba6d871e5351a1acf0d9d18b019e
Isabelle Martin <isabellemartin078@gmail.com>
2025-07-03 11:37:14 +0900

    solved customer one problem

--- 2_receipts_for_lovely_loveseat/2_receipts_for_lovely_loveseat.py
+++ 2_receipts_for_lovely_loveseat/2_receipts_for_lovely_loveseat.py
@@ -29,3 +29,24 @@ luxurious_lamp_price = 52.15
 sales_tax = .088
 
 ### Solving problems 
+
+## First customer 
+
+# Total:
+customer_one_total = lovely_loveseat_price + luxurious_lamp_price
+
+# List:
+
+customer_one_itemisation = lovely_loveseat_description + luxurious_lamp_description
+
+# Calculating tax
+
+customer_one_tax = customer_one_total * sales_tax 
+
+# Total cost:
+total_cost = customer_one_total + customer_one_tax
+
+# Printing receipt:
+
+print("Receipt: " + "Customer one items: " + customer_one_itemisation + "Customer One Total: " + str(total_cost))
+

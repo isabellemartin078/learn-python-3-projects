2f11591a1cfebd7b2348a70e6ea1c10ffd5d6ebd
Isabelle Martin <isabellemartin078@gmail.com>
2025-07-03 11:32:36 +0900

    created file for lovely loveseat project and added item descriptions

---
+++ 2_receipts_for_lovely_loveseat/2_receipts_for_lovely_loveseat.py
@@ -0,0 +1,31 @@
+## 2. Receipts for lovely loveseat
+
+# Description for lovely loveseat
+
+lovely_loveseat_description = "Lovely Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inches deep. Red or white."
+
+# Price for lovely loveseat
+
+lovely_loveseat_price = 254.00
+
+# Description for stylish settee
+
+stylish_settee_description = "Stylish Settee. Faux leather on birch. 29.50 inches high x 54.75 inches wide x 28 inches deep. Black."
+
+# Price for stylish settee
+
+stylish_settee_price = 180.50
+
+# Description for luxurious lamp
+
+luxurious_lamp_description = "Luxurious Lamp. Glass and iron. 36 inches tall. Brown with cream shade."
+
+# Price for luxurious lamp
+
+luxurious_lamp_price = 52.15
+
+## Sales tax
+
+sales_tax = .088
+
+### Solving problems 

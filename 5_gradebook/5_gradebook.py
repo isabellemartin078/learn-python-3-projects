308fb0afbd29d95700d132be22de45f8ce396cf6
Isabelle Martin <isabellemartin078@gmail.com>
2025-07-03 11:59:26 +0900

    Set up gradebook for further modification

--- 5_gradebook/5_gradebook.py
+++ 5_gradebook/5_gradebook.py
@@ -12,3 +12,19 @@ grades = [98, 97, 85, 88]
 
 gradebook = [["physics", 98], ["calculus", 97], ["poetry", 85], ["history", 88]]
 
+print(gradebook)
+
+## Updating with computer science subject and grade
+
+gradebook.append(["computer science", 100])
+
+print(gradebook)
+
+## Updating with visual arts subject and grade
+
+gradebook.append(["visual arts", 93])
+
+print(gradebook)
+
+### Modifying the gradebook
+

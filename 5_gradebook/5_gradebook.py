8c3180c21e20f79d37becd8e42a019f04c005364
Isabelle Martin <isabellemartin078@gmail.com>
2025-07-03 11:55:39 +0900

    created new folder and file for lesson 5 gradebook

---
+++ 5_gradebook/5_gradebook.py
@@ -0,0 +1,14 @@
+## 5. Gradebook
+
+# Creating a list of subjects 
+
+subjects = ["physics", "calculus", "poetry", "history"]
+
+# Creating a list of grades
+
+grades = [98, 97, 85, 88]
+
+# Creating 2D list of subjects and grades
+
+gradebook = [["physics", 98], ["calculus", 97], ["poetry", 85], ["history", 88]]
+

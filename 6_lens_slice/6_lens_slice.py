253b1058049174d1e4c04c14e72e65623546076e
Isabelle Martin <isabellemartin078@gmail.com>
2025-07-07 12:18:03 +0900

    finished problem solving

--- 6_lens_slice/6_lens_slice.py
+++ 6_lens_slice/6_lens_slice.py
@@ -17,3 +17,22 @@ print("We sell " + str(num_pizzas) + " different kinds of pizzas!")
 # Creating 2D list
 
 pizza_and_prices = [[2, "pepperoni"], [6, "pineapple"], [1, "cheese"], [3, "sausage"], [2, "olives"], [7, "anchovies"], [2, "mushrooms"]]
+print(pizza_and_prices)
+
+# Sorting and slicing pizzas
+
+pizza_and_prices.sort()
+
+cheapest_pizza = pizza_and_prices[0]
+
+priciest_pizza = pizza_and_prices[-1]
+
+pizza_and_prices.pop() # removing anchovies slice
+
+pizza_and_prices.insert(4, [2.5, "peppers"]) # adding peppers
+
+three_cheapest = pizza_and_prices[0:3]
+print("The three cheapest pizzas are " + str(three_cheapest))
+
+
+

39354f7ac890139244de4cbaa8602fba5120f54e
Isabelle Martin <isabellemartin078@gmail.com>
2025-07-07 11:47:08 +0900

    added premium shipping and drone shipping cost

--- 4_sals_shipping/4_sals_shipping.py
+++ 4_sals_shipping/4_sals_shipping.py
@@ -7,12 +7,28 @@ weight = 8.4
 # Ground shipping cost
 
 if weight <= 2:
-    cost = weight * 1.50 + 20
+    gs_cost = weight * 1.50 + 20
 elif weight > 2 and weight <= 6:
-    cost = weight * 3 + 20
+    gs_cost = weight * 3 + 20
 elif weight > 6 and weight <= 10:
-    cost = weight * 4 + 20
+    gs_cost = weight * 4 + 20
 else:
-    cost = weight * 4.75 + 20
+    gs_cost = weight * 4.75 + 20
 
-print(cost)
+print(gs_cost)
+
+# Premium shipping cost 
+
+gsp_cost = 125
+
+# Drone shipping cost 
+
+if weight <= 2:
+    ds_cost = weight * 4.50
+elif weight > 2 and weight <= 6:
+    ds_cost = weight * 9
+elif weight > 6 and weight <= 10:
+    ds_cost = weight * 12
+else:
+    ds_cost = weight * 14.25
+

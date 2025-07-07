9e406a243a2e317daa1029f5889948d79aceb97a
Isabelle Martin <isabellemartin078@gmail.com>
2025-07-07 12:01:49 +0900

    added pizza toppings and prices

---
+++ 6_lens_slice/6_lens_slice.py
@@ -0,0 +1,9 @@
+## 6. Len's slice project
+
+# Pizza toppings and prices
+
+toppings = ["pepperoni", "pineapple", "cheese", "sausage", "olives", "anchovies", "mushrooms"]
+
+prices = [2, 6, 1, 3, 2, 7, 2]
+
+# Research on $2 slices 

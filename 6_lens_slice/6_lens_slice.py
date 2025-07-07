3373a0cb7373d14ecc3f97ded7949765f04b3a15
Isabelle Martin <isabellemartin078@gmail.com>
2025-07-07 12:13:07 +0900

    added 2D list of pizza toppings and prices

--- 6_lens_slice/6_lens_slice.py
+++ 6_lens_slice/6_lens_slice.py
@@ -7,3 +7,13 @@ toppings = ["pepperoni", "pineapple", "cheese", "sausage", "olives", "anchovies"
 prices = [2, 6, 1, 3, 2, 7, 2]
 
 # Research on $2 slices 
+
+num_two_dollar_slices = prices.count(2)
+print(num_two_dollar_slices)
+
+num_pizzas = len(toppings)
+print("We sell " + str(num_pizzas) + " different kinds of pizzas!")
+
+# Creating 2D list
+
+pizza_and_prices = [[2, "pepperoni"], [6, "pineapple"], [1, "cheese"], [3, "sausage"], [2, "olives"], [7, "anchovies"], [2, "mushrooms"]]

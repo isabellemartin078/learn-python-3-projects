cfccf57c537a8521a0352a4fe5ea0f89634bf618
Isabelle Martin <isabellemartin078@gmail.com>
2025-07-07 11:42:16 +0900

    added parameters for calculating ground shipping cost

--- 4_sals_shipping/4_sals_shipping.py
+++ 4_sals_shipping/4_sals_shipping.py
@@ -1,2 +1,18 @@
 ## 4. Sal's shipping projects 
 
+# Defining weight variable 
+
+weight = 200
+
+# Ground shipping cost
+
+if weight <= 2:
+    cost = weight * 1.50 + 20
+elif weight > 2 <= 6:
+    cost = weight * 3 + 20
+elif weight > 6 <= 10:
+    cost = weight * 4 + 20
+else:
+    cost = weight * 4.75 + 20
+
+print(cost)
